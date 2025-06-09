4abcfdaa46dc2ed57a3f024b9c8558c85bc44b5a
Claudio Costa <costa.pederneschi@gmail.com>
2025-06-09 12:55:30 -0300

    Update script.js

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -83,7 +83,7 @@ const handleLogin = (event) => {
     login.style.display = "none"
     chat.style.display = "flex"
 
-    websocket = new WebSocket("wss://testsite-sadi.onrender.com˜)
+    websocket = new WebSocket("wss://testsite-sadi.onrender.com")
     websocket.onmessage = processMessage
 }
 
